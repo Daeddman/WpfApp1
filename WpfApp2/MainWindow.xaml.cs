768c90058ad52eb9e2f262a97972a55e3151ea85
Olexandr <shevchenko.oleksandr1123@vu.cdu.edu.ua>
2025-04-24 10:21:31 +0300

    Initial commit

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void EscButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            double maxX = MainCanvas.ActualWidth - NoButton.ActualWidth;
+            double maxY = MainCanvas.ActualHeight - NoButton.ActualHeight;
+            if (maxX <= 0 || maxY <= 0) return;
+
+            Point mousePos = e.GetPosition(MainCanvas);
+            double safeDistance = Math.Max(NoButton.ActualWidth, NoButton.ActualHeight);
+            double newX, newY;
+            int attempts = 0;
+
+            do
+            {
+                newX = rnd.NextDouble() * maxX;
+                newY = rnd.NextDouble() * maxY;
+                attempts++;
+            }
+            while (Math.Sqrt(
+                       Math.Pow(newX + NoButton.ActualWidth / 2 - mousePos.X, 2) +
+                       Math.Pow(newY + NoButton.ActualHeight / 2 - mousePos.Y, 2)
+                   ) < safeDistance
+                   && attempts < 100);
+
+            Canvas.SetLeft(NoButton, newX);
+            Canvas.SetTop(NoButton, newY);
+        }
+        private void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Дякую!");
+        }
+    }
+}
